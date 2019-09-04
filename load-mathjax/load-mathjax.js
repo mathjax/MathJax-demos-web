25d1a87b9c2cebad2ed5a622c481631811311fbc
Davide P. Cervone <dpvc@union.edu>
2019-09-04 19:00:42 -0400

    Update address to use https

--- load-mathjax/load-mathjax.js
+++ load-mathjax/load-mathjax.js
@@ -9,7 +9,7 @@
       };
     }
     var script = document.createElement('script');
-    script.src = 'http://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
     document.head.appendChild(script);
   }
 })();
