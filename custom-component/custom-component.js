68934f47057ea3a48ba6048e5f4776962bbe0e77
Davide P. Cervone <dpvc@union.edu>
2020-07-06 10:28:33 -0400

    Update custom-component

--- custom-component/custom-component.js
+++ custom-component/custom-component.js
@@ -9,21 +9,33 @@ require('mathjax-full/components/src/startup/lib/startup.js');
 //
 const {Loader} = require('mathjax-full/js/components/loader.js');
 Loader.preLoad(
-    'loader', 'startup',
-    'core',
-    'input/tex-base',
-    '[tex]/newcommand',
-    '[tex]/action',
-    'output/chtml', 'output/chtml/fonts/tex.js'
+  'loader', 'startup',
+  'core',
+  'input/tex-base',
+  '[tex]/ams',
+  '[tex]/newcommand',
+  '[tex]/configmacros',
+  '[tex]/action',
+  'output/chtml', 'output/chtml/fonts/tex.js',
+  'ui/menu'
 );
 
 //
-// Because the root URL will be determined from the URL of the
-// combined file we load into the web pasge (this file), we
-// adjust the mathjax path to be the CDN instead of this directory.
+// Update the configuration to include any needed values
+// (we set the mathjax path explicitly, since it defaults
+//  to the location from which this file is loaded)
 //
-const paths = MathJax.config.loader.paths;
-paths.mathjax = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5';
+const {insert} = require('mathjax-full/js/util/Options.js');
+insert(MathJax.config, {
+  loader: {
+    paths: {
+      mathjax: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5'
+    }
+  },
+  tex: {
+    packages: {'[+]': ['ams', 'newcommand', 'configmacros', 'action']}
+  }
+}, false);
 
 //
 // Load the components that we want to combine into one component
@@ -32,15 +44,18 @@ paths.mathjax = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5';
 require('mathjax-full/components/src/core/core.js');
 
 require('mathjax-full/components/src/input/tex-base/tex-base.js');
+require('mathjax-full/components/src/input/tex/extensions/ams/ams.js');
 require('mathjax-full/components/src/input/tex/extensions/newcommand/newcommand.js');
+require('mathjax-full/components/src/input/tex/extensions/configmacros/configmacros.js');
 require('mathjax-full/components/src/input/tex/extensions/action/action.js');
-MathJax.config.tex.packages.push('newcommand', 'action'); // add these to the tex packages to register
 
 require('mathjax-full/components/src/output/chtml/chtml.js');
 require('mathjax-full/components/src/output/chtml/fonts/tex/tex.js');
 
+require('mathjax-full/components/src/ui/menu/menu.js');
+
 //
-// Loading this component will cause all the normal startup operations to be
-//   performed when this component is loaded.
+// Loading this component will cause all the normal startup
+//   operations to be performed when this component is loaded
 //
 require('mathjax-full/components/src/startup/startup.js');
